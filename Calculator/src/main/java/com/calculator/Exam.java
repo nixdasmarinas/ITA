e414b0a8ba8aee5c8ad6592d0b00c3a1c17477c9
nixdasmarinas <mddasmarinas@up.edu.ph>
2017-08-04 17:27:18 +0800

    Test Exam and Calculator

---
+++ Calculator/src/main/java/com/calculator/Exam.java
@@ -0,0 +1,78 @@
+package com.calculator;
+
+import java.util.Scanner;
+
+public class Exam {
+
+	public static void main(String[] args) {
+		System.out.println("========== ONE ================");
+		number1();
+		System.out.println("========== TWO ================");
+		System.out.println(number2(8));
+		System.out.println("========== THREE ================");
+		System.out.println(number3());
+		System.out.println("========== FOUR ================");
+		System.out.println(number4(3,10));
+		
+	}
+
+	public static String number1() {
+		int i = 1;
+		for (int j = 0; j < 5; j = j + 2) {
+			i = (i * i) + j;
+		}
+		switch (i) {
+		case 3:
+			System.out.println("zero");
+			return "zero";
+		case 13:
+			System.out.println("one");
+			return "one";
+		case 175:
+			System.out.println("two");
+			return "two";
+		default:
+			System.out.println("default");
+			return "default";
+		}
+	}
+
+	public static int number2(int n) {
+		int F_0 = 1, F_1 = 1;
+		int F_n = 0;
+		/*System.out.println("Enter the number");
+		Scanner s = new Scanner(System.in);
+		
+		n = s.nextInt(); */
+		for (int i = 1; i < n; i++) {
+			F_n = F_0 + F_1;
+			F_0 = F_1;
+			F_1 = F_n;
+		}
+		return F_n;
+	}
+
+	public static int number3() {
+		int inner = 2, outer = 2;
+		int result = 0;
+		for (int i = 0; i < outer; i++) {
+			for (int j = 0; j < inner; j++) {
+				result = (result * result) + 1;
+			}
+		}
+		return result;
+	}
+	
+	public static int number4(int x, int y) {
+		int count = 0;
+		while (x < y)
+		{
+			System.out.println ("Hello");
+			x = x + 1;
+			y = y - 1;
+			count++;
+		}
+		return count;
+	}
+	
+}
---
+++ Calculator/src/test/java/TestExam.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.calculator.Exam;
+
+public class TestExam {
+
+	@Test
+	public void test() {
+		/* NUMBER 1 */
+		assertEquals("one",Exam.number1());
+		
+		/* NUMBER 2 */
+		assertEquals(8,Exam.number2(5));
+		assertEquals(34,Exam.number2(8));
+		assertNotNull(Exam.number2(5));
+		assertNotSame(32,Exam.number2(7));
+		assertNotEquals(32,Exam.number2(7));
+		
+		/* NUMBER 3 */
+		
+		
+		
+	}
+
+}
